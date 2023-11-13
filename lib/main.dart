06913c6f7ec4b451964e7d57d889c8edeb296502
srinithi <srinithi250104@gmail.com>
2023-11-13 22:12:54 +0530

    Final Commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:listy_app/gradient_container.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: GradientContainer(Color.fromARGB(255, 33, 5, 109), Color.fromARGB(255, 68, 21, 149)),
+      ),
+    ),
+  );
+}
